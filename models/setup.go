93d92501529d9885090b20af83af9a5137cce2b7
MHafizAF <hafiz170301@gmail.com>
2023-06-02 14:28:25 +0700

    setup database for authentication

--- models/setup.go
+++ models/setup.go
@@ -1,6 +1,11 @@
 package models
 
 import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -8,13 +13,31 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	database, err := gorm.Open(mysql.Open("root:@tcp(localhost:3306)/go_bookself"))
+	error := godotenv.Load(".env")
 
-	if err != nil {
-		panic(err)
+	if error != nil {
+		log.Fatalf("Error loading .env file")
 	}
 
-	database.AutoMigrate(&Book{})
+	DbDriver := os.Getenv("DB_DRIVER")
+	DbHost := os.Getenv("DB_HOST")
+	DbUser := os.Getenv("DB_USER")
+	DbPassword := os.Getenv("DB_PASSWORD")
+	DbName := os.Getenv("DB_NAME")
+	DbPort := os.Getenv("DB_PORT")
 
-	DB = database
+	DBURL := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", DbUser, DbPassword, DbHost, DbPort, DbName)
+
+	DB, error = gorm.Open(mysql.Open(DBURL))
+
+	if error != nil {
+		fmt.Println("Cannot connect to database", DbDriver)
+		log.Fatal("Connection error : ", error)
+	} else {
+		fmt.Println("We are connected to the database ", DbDriver)
+	}
+
+	DB.AutoMigrate(&User{})
+	DB.AutoMigrate(&Book{})
+
 }
